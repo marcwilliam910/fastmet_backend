203ee407ec8dbbe3a25be809767d6673e8e719f1
unknown <marcwilliamvaliente910@gmail.com>
2025-11-26 14:27:50 +0800

    feat(bookings): make getBookings endpoint paginated

--- src/controllers/client/bookingController.ts
+++ src/controllers/client/bookingController.ts
@@ -3,7 +3,25 @@ import BookingModel from "../../models/Booking";
 
 export const getBookings: RequestHandler = async (req, res) => {
   const { userId } = req.params;
+  const { status, page = 1, limit = 5 } = req.query;
 
-  const bookings = await BookingModel.find({ userId }).sort({ createdAt: -1 });
-  res.status(200).json(bookings);
+  if (!userId) {
+    return res.status(400).json({ message: "Missing user ID" });
+  }
+
+  const pageNum = Number(page);
+  const limitNum = Number(limit);
+
+  const bookings = await BookingModel.find({ userId, status })
+    .sort({ createdAt: -1 })
+    .skip((pageNum - 1) * limitNum)
+    .limit(limitNum);
+
+  // Get total count to know if there are more pages
+  const total = await BookingModel.countDocuments({ userId, status });
+
+  res.status(200).json({
+    bookings,
+    nextPage: pageNum * limitNum < total ? pageNum + 1 : null,
+  });
 };
