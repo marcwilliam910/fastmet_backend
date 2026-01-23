c9cab045eee0a108884cbb00a4e3b12c8ac6df0e
marcwilliam910 <marcwilliamvaliente910@gmail.com>
2026-01-23 10:41:08 +0800

    feat(api): add vehicle key as response on getVehicles

--- src/controllers/vehicleController.ts
+++ src/controllers/vehicleController.ts
@@ -1,16 +1,18 @@
-import { RequestHandler } from "express";
-import { VehicleType } from "../models/Vehicle";
+import {RequestHandler} from "express";
+import {VehicleType} from "../models/Vehicle";
 
 export const getVehicles: RequestHandler = async (req, res) => {
   const vehicleTypes = await VehicleType.find();
 
   if (!vehicleTypes || vehicleTypes.length === 0) {
-    return res.status(404).json({ message: "Vehicle types not found" });
+    return res.status(404).json({message: "Vehicle types not found"});
   }
 
   // Return just names if requested
   if (req.query.fields === "name") {
-    return res.json(vehicleTypes.map((v) => ({ value: v._id, label: v.name })));
+    return res.json(
+      vehicleTypes.map((v) => ({value: v._id, label: v.name, key: v.key})),
+    );
   }
 
   res.json(vehicleTypes);
